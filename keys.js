b152b7796619e2875c4f549fad42c6e6a5547efd
Larisasmiles <larisasmiles@gmail.com>
2018-01-26 06:12:41 -0700

    linking files

---
+++ keys.js
@@ -0,0 +1,11 @@
+JavaScript
+console.log('this is loaded');
+
+var twitterKeys = {
+  consumer_key: '<hafBAl8racwrnAYsjnt6U9Kch>',
+  consumer_secret: '<lvN0pec7gL8nhGhtMtzNU6DQZ7fIlm8ztLqBzVtnLj9tVxlhl7>',
+  access_token_key: '<57360445-WLft3dQ16uQLltB14sTY1VBeDp0nkm126gzzB6MOp>',
+  access_token_secret: '<v65hQOeC0jbRGenJ3jHMlbEBG2WlhNF15bzokexuaOvTx>',
+}
+
+module.exports = twitterKeys;
---
+++ liri.js
@@ -0,0 +1,23 @@
+`my-tweets`
+`spotify-this-song`
+`movie-this`
+`do-what-it-says`
+
+
+1. `node liri.js my-tweets`
+
+   * This will show your last 20 tweets and when they were created at in your terminal/bash window.
+
+2. `node liri.js spotify-this-song '<song name here>'`
+
+   * This will show the following information about the song in your terminal/bash window
+     
+     * Artist(s)
+     
+     * The song's name
+     
+     * A preview link of the song from Spotify
+     
+     * The album that the song is from
+
+   * If no song is provided then your program will default to "The Sign" by Ace of Base.
